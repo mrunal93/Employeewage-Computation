a417f130adff18349613fc51a206e22940903d32
mrn26893@gmail.com <mrn26893@gmail.com>
2020-06-23 07:27:40 +0530

    Employee In java

---
+++ Employeewage.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+class Employeewage {
+	public static void main(String arg[]){
+		Random rand = new Random();
+		int Present = 2;
+		int int_random = rand.nextInt(Present);
+		System.out.println("Random number is " +int_random);
+
+	}
+
+
+}
