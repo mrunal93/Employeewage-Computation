eb6dd117309c7ae85a301ce4292f9356afdbd169
mrn26893@gmail.com <mrn26893@gmail.com>
2020-06-23 08:22:47 +0530

    parttime

--- Employeewage.java
+++ Employeewage.java
@@ -3,19 +3,25 @@ class Employeewage {
 	public static void main(String arg[]){
 		System.out.println("Welcome to Employee Wage Computation");
 		Random rand = new Random();
-		int Present = 2;
+		int Present = 3;
 		int WagePerHours = 20;
-		int FullDayHours = 8;
+		int DayHours = 8;
+		int parttime=4;
 		int int_random = rand.nextInt(Present);
-
 		if( int_random == 1){
 			System.out.println("Employee is Present");
-			int DailyWage=FullDayHours*WagePerHours;
-			System.out.println("Wages:"+DailyWage);
-		}
+			int DailyWage=DayHours*WagePerHours;
+			System.out.println("Wages:" +DailyWage);
+			}
+		else if ( int_random == 2){
+			System.out.println("Employee HalfDay");
+			int DailyWage=parttime*WagePerHours;
+			System.out.println("Wages: "+DailyWage);
+			}
+
 		else{
 			System.out.println("Employee is Absent");
-		}
+			}
 
 	}
 
